44f42e7667621fe7ce3ceb17fec070de277cf396
kpgoing <xubowei96@gmail.com>
2016-10-15 12:56:07 +0800

    change : main class name.

--- src/main/java/org/sel/rms/RMSApplication.java
+++ src/main/java/org/sel/rms/RMSApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class LearnBootOneApplication {
+public class RMSApplication {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnBootOneApplication.class, args);
+		SpringApplication.run(RMSApplication.class, args);
     }
 }
