da0348f688ce2b72c1b6c6ca124904e99dbdf5e1
黄俊钦 <uestc_ccse@qq.com>
2016-11-04 15:01:40 +0800

    论文详情

---
+++ frontend/js/t_detail.js
@@ -0,0 +1,15 @@
+$(function(){
+	$(".header_nav_link").click(function (e) {
+		var index = $(this).index();
+		if(index-2 == 0){
+			$(".menu").eq(1).hide();
+		}else{
+			$(".menu").eq(0).hide();
+		}
+       	$(".menu").eq(index-2).toggle();
+       	e.stopPropagation();
+   });
+    $(document).click(function () {
+       	$(".menu").hide();
+    });
+});
