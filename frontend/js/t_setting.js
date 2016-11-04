27b8829c096716f69e2425bcc152a9e8be8fb41a
李悦 <1141628710@qq.com>
2016-11-04 22:24:18 +0800

    个人信息页面完善

---
+++ frontend/js/t_setting.js
@@ -0,0 +1,18 @@
+/**
+ * Created by 李悦 on 2016/11/4.
+ */
+$(function(){
+    $(".header_nav_link").click(function (e) {
+        var index = $(this).index();
+        if(index-2 == 0){
+            $(".menu").eq(1).hide();
+        }else{
+            $(".menu").eq(0).hide();
+        }
+        $(".menu").eq(index-2).toggle();
+        e.stopPropagation();
+    });
+    $(document).click(function () {
+        $(".menu").hide();
+    });
+})
